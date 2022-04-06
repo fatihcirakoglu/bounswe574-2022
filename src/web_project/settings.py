4da8761d2084e5ad3d26fcaae7ac2b91c20eec7c
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-04-07 01:46:34 +0300

    feat: Added delivery for milestone 1

--- src/web_project/settings.py
+++ src/web_project/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     'default': {
