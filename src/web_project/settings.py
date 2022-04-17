61646799906568cae1ebd2735aefd6fe470c5a70
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-04-18 02:13:30 +0300

    fix: corrected password change and reset page rendering problem
    -Addd password reset email tamplate
    -Fixed rendering problem for password reset and change templates

--- src/web_project/settings.py
+++ src/web_project/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -132,8 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
 EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = 587
