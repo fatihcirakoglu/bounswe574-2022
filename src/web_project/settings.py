f3d293f6045af45305ff57b5e1d0b65a2506209c
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-05-23 00:42:41 +0300

    feat: added new html files for new page formats -Added description.html to show welcome message -Added detail.html to show post details -Added favourites.html to show user's joined and owner posts -Added header.html to show links for login, signup, colearning spaces, logout -Added postbytag.html to show posts under a specific tag -Added pprofile-update.html to show user profile update features -Added profile.html to show user profile details

--- src/web_project/settings.py
+++ src/web_project/settings.py
@@ -120,21 +120,24 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 EMAIL_HOST = "smtp.mailgun.org"
