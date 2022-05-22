2529b19b61861ec9d8293a13b96edfb0f4a484de
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-05-23 02:54:59 +0300

    feat: added signup, login, profile and comment forms

--- src/webapp/forms.py
+++ src/webapp/forms.py
@@ -1,7 +1,39 @@
-# users/forms.py
-
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Comment, Profile, Post
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ("email",)
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'email')
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',  
+            'email', 
+        ]
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'profile_image'
+        ]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
+        
+        widgets={
+
+            'name':forms.TextInput(attrs={'class' : 'form-control'}),
+            'body':forms.Textarea(attrs={'class' : 'form-control'})
+
+        }
