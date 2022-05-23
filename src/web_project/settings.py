702139b3843a85401f0d6ecd770fc0babb6ecb99
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-05-23 19:31:00 +0300

    fix: read allowed host from env variables to make secure access to website -Make allowed hosts setting configurable to ease deployment in production -Increase security to restrict acess to specifi host

--- src/web_project/settings.py
+++ src/web_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('ALLOWEDHOST')]
 
 
 # Application definition
