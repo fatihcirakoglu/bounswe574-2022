0ada8e7bc70e7a28b27c4753898fe5250d554464
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-05-29 13:05:12 +0300

    fix: correction of smtp email settings for password reset

--- src/web_project/settings.py
+++ src/web_project/settings.py
@@ -142,13 +142,12 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND  = 'django.core.mail.backends.smtp.EmailBackend'
 
-
-EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_HOST = "smtp-relay.sendinblue.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 #Default one
