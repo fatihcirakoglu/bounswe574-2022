04b09d419abdc81a0eba026b296baad4789f3e38
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-12-13 20:05:56 +0000

    Annotator store is working from 5000 port.

--- annotator-store-service/run.py
+++ annotator-store-service/run.py
@@ -106,7 +106,7 @@ def main(argv):
 
     app.register_blueprint(store.store)
 
-    host = os.environ.get('HOST', '127.0.0.1')
+    host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
     app.run(host=host, port=port)
 
